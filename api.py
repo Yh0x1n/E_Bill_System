b3b5bd40d69f8c1685e4f60011533cb5e036f3fd
Yh0x1n <yhoxrossell508@gmail.com>
2024-09-18 14:42:49 -0400

    Applying functions

--- api.py
+++ api.py
@@ -6,3 +6,11 @@ import pandas as pd
 import sqlite3 as sqlconn
 import os
 import sys
+
+flux = FastAPI()
+
+@flux.get("/")
+def index():
+    return "Hello World"
+
+#TO-DO: implementar funciones de subida de archivos a la nube
