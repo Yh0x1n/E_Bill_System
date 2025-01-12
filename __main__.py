3635191aaba75ebb3b2cbccd2c5a786050d9be62
Yh0x1n <yhoxrossell508@gmail.com>
2025-01-12 14:18:29 -0400

    Starting from scratch

--- __main__.py
+++ __main__.py
@@ -3,8 +3,7 @@
 #Importaciones
 import sqlite3 as sqlconn, pandas as pd, os, sys, random
 from PySide6 import QtWidgets, QtCore, QtGui
-
-#Queremos empezar a usar FastAPI para implementar un backend web en el que se puedan subir archivos a la nube
+import reportlab
 
 #Clase principal
 class App(QtWidgets.QWidget): #Esta clase es la ventana principal, que muestra los datos del dataframe al iniciar
