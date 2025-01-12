7ddedc5716086261301abedca20da903caa2997c
Yh0x1n <yhoxrossell508@gmail.com>
2025-01-12 14:20:59 -0400

    a

--- __main__.py
+++ __main__.py
@@ -1,9 +1,9 @@
 #Aplicación de gestión de inventario utilizando SQL y Pandas
 
 #Importaciones
-import sqlite3 as sqlconn, pandas as pd, os, sys, random
+import sqlite3 as sql, pandas as pd, os, sys, random
 from PySide6 import QtWidgets, QtCore, QtGui
-import reportlab
+import reportlab, qrcode, Crypto
 
 #Clase principal
 class App(QtWidgets.QWidget): #Esta clase es la ventana principal, que muestra los datos del dataframe al iniciar
@@ -12,7 +12,7 @@ class App(QtWidgets.QWidget): #Esta clase es la ventana principal, que muestra l
 
         #Conectar con la base de datos
         try:
-            conn = sqlconn.connect(
+            conn = sql.connect(
                 #host = "localhost",
                 #user = "root",
                 #passwd = "",
@@ -23,5 +23,5 @@ class App(QtWidgets.QWidget): #Esta clase es la ventana principal, que muestra l
             cur.execute('CREATE TABLE IF NOT EXISTS productos (id INTEGER PRIMARY KEY, nombre TEXT, precio REAL, stock INTEGER);')
             conn.commit()
         
-        except sqlconn.Error as e:
+        except sql.Error as e:
             print(e)
