b628c48c9e0c51e9b35dbd1d574c1dd5f09fe5f6
Yh0x1n <yhoxrossell508@gmail.com>
2025-01-12 14:10:06 -0400

    Starting from scratch

--- api.py
+++ api.py
@@ -1,16 +1,3 @@
 '''
 En este script irá el backend de la aplicación, donde se subirán archivos a la nube
 '''
-from fastapi import FastAPI
-import pandas as pd
-import sqlite3 as sqlconn
-import os
-import sys
-
-flux = FastAPI()
-
-@flux.get("/")
-def index():
-    return "Hello World"
-
-#TO-DO: implementar funciones de subida de archivos a la nube
